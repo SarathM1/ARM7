14672b4fe925e54822a88d62c0aceaa433381871
Sarath M <sarathm00@gmail.com>
2015-09-28 23:00:22 +0530

    Renamed files

--- GPS/gps.c
+++ GPS/gps.c
@@ -1,23 +1,18 @@
 #include "uart0_inter.h"
 #include "lcd.h"
 
-int flagD = 0;
-int comma = 0;
+char string[]="GPRMC";
+
 int gprmc = 0;
-char flagPack[]="GPRMC";
-
-char latitude[15];
-char longitude[15];
-int x=0;
-int y=0;
-char NS;
-char EW;
-
-int dataValid = 0;
-int latValid=0;
+int comma  = 0;
+int dataValid = -1;
+int x = 0;
+char latitude[10];
+int latValid = 0;
+char ns=0;
+char longitude[11];
 int longValid = 0;
-int nsValid =0;
-int ewValid	= 0;
+char ew=0;
 
 void uart(void)__irq // ISR for UART0
 {
@@ -27,122 +22,122 @@ void uart(void)__irq // ISR for UART0
 	U0THR = ch;
 	while(!(U0LSR&0x20));
 
+	
+
 	if(ch=='$')
 	{
-		flagD =1;
-		comma=0;
-		gprmc=0;
-		x=0;	
-		y=0;
-		dataValid = 0;
-		latValid=0;
-		longValid = 0;
-		nsValid =0;
-		ewValid	= 0;
-	}
+		cmd(0x01);
+		delay(100);
 
-	if( (ch == flagPack[gprmc]) && (flagD == 1) )
+		gprmc = 0;
+		comma = 0;
+		dataValid = -1;
+		latValid=0;
+		x=0;
+		ns=0;
+		longValid=0;
+		ew=0;
+	}
+	
+	if(string[gprmc]==ch)
 	{
 		gprmc++;
 	}
 
-	if(gprmc==5)
+	if(ch==',')
 	{
-		if(ch == ',')
-		{
-			comma++;
-		}
-
-		switch(comma)
-		{
-			case 0:case 1:case 2:break;
-
-			case 3: 
-				if(ch=='A')
-				{
-					dataValid = 1;				
-				}
-				else 
-				{
-					dataValid = 0;
-				}
-				break;
-
-			case 4:
-				latitude[x++]=ch;
-				latitude[x+1]='\0';
-				break;
-
-			case 5:
-				NS = ch;
-				break;
-
-			case 6:
-				longitude[y++]=ch;
-				longitude[y+1]='\0';
-				break;
-
-			case 7:
-				EW = ch	;
-				break;	
-			
-			case 8:case 9:case 10:case 11:case 12:
-				break;
-			
-			default:
-				flagD=0;		
-				break;
-		
-		}
+		comma++;
 	}
 
-	VICVectAddr = 0;
+	switch(comma)
+	{
+		case 2:
+			if(ch=='A')
+				dataValid=1;
+			else if(ch=='V')
+				dataValid=0;
+			break;
+		
+		case 3:
+			if(ch!=',')
+			{
+				latitude[x++]=ch;
+				latitude[x+1]='\0';
+			}
+			break;
+		
+		case 4:
+			latValid = 1;
+			x=0;			//Reset x to reuse for longitude
+			if(ch!=',')
+			{
+				ns=ch;
+			}
+			break;
+		
+		case 5:
+			if(ch!=',')
+			{
+				longitude[x++]=ch;
+				longitude[x+1]='\0';
+			}
+			break;
+		
+		case 6:
+			longValid = 1;
+			if(ch!=',')
+			{
+				ew=ch;
+			}
+			break;
+	}
 }
-
 
 int main()
 {
-	int i;
+	
 	uart_init();
 	lcd_init();
-	
 
 	while(1)
 	{
-		if(!dataValid)
+		if(gprmc==5)
 		{
-			cmd(0x01);
-			lcd_str("Data Invalid!!");		
-		}
-		
-		else
-		{
+			cmd(0x80);
+			lcd_str("G ");
+
+			if(dataValid==1)
+			{
+				cmd(0x82);
+				lcd_str("A ");
+			}
+			else if(dataValid==0)
+			{
+				cmd(0x82);
+				lcd_str("V ");
+			}
+
 			if(latValid)
 			{
-				cmd(0x80);
-		 		for(i=0;i<9;i++)
-					lcd_char(latitude[i]);
+				cmd(0x84);
+				lcd_str(latitude);  
 			}
-			
-			if(nsValid)
+
+			if(ns!=0)
 			{
-			
-				cmd(0x8C);
-				lcd_char(NS);
+				lcd_char(ns);
 			}
 
 			if(longValid)
 			{
 				cmd(0xc0);
-				for(i=0;i<9;i++)
-					lcd_char(longitude[i]);
+				lcd_str(longitude);
 			}
-			if(ewValid)
+
+			if(ew!=0)
 			{
-				cmd(0xcc);
-			 	lcd_char(EW);
+				lcd_char(ew);
 			}
 		}
 	}
-	
 }
