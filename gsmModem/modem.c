44cc55af94de40f5d2a0d73dd64ed9bd2218d743
Sarath M <sarathm00@gmail.com>
2015-09-28 15:19:58 +0530

    Code Working

--- gsmModem/modem.c
+++ gsmModem/modem.c
@@ -2,17 +2,24 @@
 #include "lcd.h"
 #include "uart0_inter.h"
 
+int nextAt=0;
 void uart(void)__irq // ISR for UART0
 {
 	char ch;
 	ch = U0RBR;
 	
-	U0THR = ch; 		// Echo what is recieved
-	while( !(U0LSR & 0x20) );
-
 	if(ch == resp[flag])
+	{	
+		lcd_char(ch);
 		flag++;
-
+	}
+	
+	if(flag == 2)
+	{
+		flag = 0;
+		nextAt++;	
+	}
+	
 	VICVectAddr = 0;
 }
 
@@ -26,22 +33,28 @@ void sendAt(char *cmd)
 		uart_tx_char(cmd[i]);
 	}
 	uart_tx_str("\r\n");
-	delay(100);
-
-	while(flag!=2);
-	
-	flag=0;
+	delay(250);
 	
 }
 
 int main()
 {
 	uart_init();
-	
+	lcd_init();
+
 	while(1)
 	{
-		sendAt("AT");
-		sendAt("ATE0");
+	
+		switch(nextAt)
+		{
+			case 0: sendAt("AT");
+					break;
+			case 1: sendAt("ATE0");
+					break;
+			case 2: sendAt("AT+CMGF=1");
+					break;					
+		}
+			
 	}
 
 }
