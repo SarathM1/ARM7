c1210144e427294b71e4906d09c2ff0f84e0de27
Sarath M <sarathm00@gmail.com>
2015-10-01 17:22:51 +0530

    Working

---
+++ RTC/main.c
@@ -0,0 +1,47 @@
+#include<lpc213x.h>
+#include "lcd.h"
+
+void rtc(void)__irq;	  //Fn Declaration 
+
+void rtc_init()
+{
+	
+	ILR = 0X01;
+	CCR = 0X13;
+	CCR = 0X11;
+	CIIR = 0X01;
+
+	VICIntEnable = 0x00002000; // Enable RTC Interrupt
+	VICIntSelect = 0x00000000; // Set RTC interrupt as IRQ
+	VICVectCntl0 = 0x2D;	// Asign 13 for RTC and set IRQslot_en
+	VICVectAddr0 = (unsigned)rtc;	 // Address for ISR
+
+}
+
+void reset_time()
+{
+	HOUR 	= 0;
+	MIN		= 0;
+	SEC		= 0;
+	
+}
+
+
+void rtc(void)__irq // ISR for UART0
+{
+	static int i=0;
+	i++;
+	//cmd(0x80);
+	lcd_char('a');
+	ILR = 0X01;
+	VICVectAddr = 0;	// Compulsary in all ISR's
+}
+
+int main()
+{
+	lcd_init();
+	rtc_init();
+	reset_time();
+	while(1);
+}			   
+
