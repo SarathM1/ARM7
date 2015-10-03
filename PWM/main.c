2b99ea06c1783e6555bac008bcaa55ff17856939
Sarath M <sarathm00@gmail.com>
2015-10-03 20:40:48 +0530

    PWM Working

--- PWM/main.c
+++ PWM/main.c
@@ -9,22 +9,21 @@ void delay(int x)
 
 void pwm_init()	
 {
-	PINSEL0 = 0x02; 	// Select PWM1 on P0.0 
-	PWMTCR 	= 0x02;		// Enable PWM, Enable counter, 
-
-	PWMMR0 	= 15000;	// Value to generate PWM of 500 ms Time period
-	
+	PINSEL0 = (1<<1); 	// Select PWM1 on P0.0 
+	PWMPR	= 59;
 	/*
-		reqd time (ms) = TMMR0 / PCLK(MHz)
-		
-		here PCLK = 30 MHz	
-	*/ 	
+		PWMPR = (PWMPR+1)/(PCLK*10^6)
+		Taking PCLk = 60 (60 MHz)
+	*/
+	PWMMR0	= 10000;	// 10 ms time period
+	PWMMR1	= 2500 ;	// T-ON = 25%
+	PWMMCR 	= (1<<1);		// Reset PWMTC on PWMMR0 match
+	PWMLER	= (1<<1)|(1<<0);	// update PWMMR0 and PWMMR1
+	PWMPCR	= (1<<9);	// Enable PWM output
+	PWMTCR 	= (1<<1);	// Reset PWM TC and PR
+	PWMTCR  = (1<<0)|(1<<3);		// Enable counter's and PWM mode  
 
-	PWMMR1 	= 15000;  // Make duty cycle=100%, Duty cycle = PRMMR1 / PWMMR0 
-	PWMMCR 	= 0X02;		// Reset PWM1 on match
-	PWMPCR	= 0x0200;	// Enable PWM
-	PWMLER	= 0X03;		// Load values to PWMMR0 and PWMMR1 on next match
-	PWMTCR  = 0x09;		// Reset PWM Timer Counter and PWM Prescalat counter on match  
+	
 }
 
 
@@ -32,18 +31,19 @@ int main()
 {
 	pwm_init();
 	//delay(500);
+	/*
 	while(1)
 	{
-		PWMMR1 	-= 1000;
+		PWMMR1 	-= 1;
 		
 		if(PWMMR1 <= 0)
-			PWMMR1 = 15000;
+			PWMMR1 =6 ;
 		
-		PWMLER	= 0X02;		// Load values to PWMMR1 on next match
+		PWMLER	= 0X03;		// Load values to PWMMR1 on next match
 		delay(500);
 		
 	}
-	
-	//while(1);
+	  */
+	while(1);
 
 }
