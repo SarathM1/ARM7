0b748e4028077b813a13ef3cb1f03b9d7cacdaf4
Sarath M <sarathm00@gmail.com>
2015-10-06 23:36:06 +0530

    Renamed all registers -> ex:  I2CONSET -> I2C0CONSET

--- i2c/eeprom.c
+++ i2c/eeprom.c
@@ -4,65 +4,65 @@
 void i2c_init()
 {
 	PINSEL0 = (1<<4)|(1<<6);
-	I2CONCLR = 0XFF;   // Clear all bits of I2CONSET
-	I2CONSET = 0X40;	// Enable I2C, I2EN =1
-	I2SCLH	= 0X96;
-	I2SCLL	= 0X96;
+	I2C0CONCLR = 0X2C;   //Clear AA, I2C Interrupt Flag and Start Bit	 #
+	I2C0CONSET = (1<<6)|(1<<2);	// I2EN =1, AA=1					#
+	I2C0SCLH	= 0X96;
+	I2C0SCLL	= 0X96;
 }
 
 void i2c_start()
 {
-	I2CONSET = (1<<2)|(1<<5); // AA=1 , STA =1
-	I2CONCLR = (1<<3);	// SIC = 1
-	while(I2STAT!=0X08); // wait for acknowlege from slave
+	I2C0CONSET = (1<<2)|(1<<5); // AA=1 , STA =1
+	I2C0CONCLR = (1<<3);	// SIC = 1
+	while(I2C0STAT!=0X08); // wait for acknowlege from slave
 
 
 }
 
 void i2c_addr(char slave_addr)
 {
-	I2CONSET = (1<<2); // AA =1
-	I2CONCLR = (1<<3)|(1<<5);	// SIC = 1
-	I2DAT = slave_addr;
-	while(I2STAT != 0X18);	
+	I2C0CONSET = (1<<2); // AA =1
+	I2C0CONCLR = (1<<3)|(1<<5);	// SIC = 1
+	I2C0DAT = slave_addr;
+	while(I2C0STAT != 0X18);	
 }
 
 void i2c_addr2(char slave_addr)
 {
-	I2CONSET = (1<<2); // AA =1
-	I2CONCLR = (1<<3);	// SIC = 1
-	I2DAT = slave_addr;
-	while(I2STAT != 0X40);
+	I2C0CONSET = (1<<2); // AA =1
+	I2C0CONCLR = (1<<3);	// SIC = 1
+	I2C0DAT = slave_addr;
+	while(I2C0STAT != 0X40);
 }
 
 void i2c_write(char data)
 {
-	I2CONSET = (1<<2);
-	I2CONCLR = (1<<3);	// SIC = 1
-	I2DAT 	= data;
-	while(I2STAT != 0X28);
+	I2C0CONSET = (1<<2);
+	I2C0CONCLR = (1<<3);	// SIC = 1
+	I2C0DAT 	= data;
+	while(I2C0STAT != 0X28);
 }
 
 char i2c_read()
 {
-	I2CONSET = (1<<2);
-	I2CONCLR = (1<<3);	// SIC = 1
-	while(I2STAT != 0x50);
-	return I2DAT;
+	I2C0CONSET = (1<<2);
+	I2C0CONCLR = (1<<3);	// SIC = 1
+	while(I2C0STAT != 0x50);
+	return I2C0DAT;
 }
 void i2c_location(char loc_addr)
 {
-	I2CONSET = (1<<2);
-	I2CONCLR = (1<<3)|(1<<5);	// SIC = 1 , Clear ST0 ??
-	I2DAT 	= loc_addr;
-	while(I2STAT != 0X28);
+	I2C0CONSET = (1<<2);
+	I2C0CONCLR = (1<<3)|(1<<5);	// SIC = 1 , Clear ST0 ??
+	I2C0DAT 	= loc_addr;
+	while(I2C0STAT != 0X28);
 
 }
 
 void i2c_stop()
 {
-	I2CONSET = (1<<2) | (1<<4);	 // AA = 1, STO = 1
-	I2CONCLR = (1<<3);			//Clear SIC
+	I2C0CONSET = (1<<2) | (1<<4);	 // AA = 1, STO = 1
+	I2C0CONCLR = (1<<3);			//Clear SIC
 
 }
 
