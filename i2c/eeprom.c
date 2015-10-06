d5ffada10389466c6dc6ae26cba84bf88a693515
Sarath M <sarathm00@gmail.com>
2015-10-06 23:41:23 +0530

    Renamed all functions

--- i2c/eeprom.c
+++ i2c/eeprom.c
@@ -1,7 +1,7 @@
 #include<lpc21xx.h>
 #include "lcd.h"
 
-void i2c_init()
+void i2c0_init()
 {
 	PINSEL0 = (1<<4)|(1<<6);
 	I2C0CONCLR = 0X2C;   //Clear AA, I2C Interrupt Flag and Start Bit	 #
@@ -10,16 +10,19 @@ void i2c_init()
 	I2C0SCLL	= 0X96;
 }
 
-void i2c_start()
+void i2c0_start()
 {
-	I2C0CONSET = (1<<2)|(1<<5); // AA=1 , STA =1
-	I2C0CONCLR = (1<<3);	// SIC = 1
+	I2C0CONSET = (1<<5); // STA =1 				#
 	while(I2C0STAT!=0X08); // wait for acknowlege from slave
-
-
 }
 
-void i2c_addr(char slave_addr)
+void i2c0_stop()
+{
+	I2C0CONSET = (1<<2) | (1<<4);	 // AA = 1, STO = 1
+	I2C0CONCLR = (1<<3);			//Clear SIC
+}
+
+void i2c0_addr(char slave_addr)
 {
 	I2C0CONSET = (1<<2); // AA =1
 	I2C0CONCLR = (1<<3)|(1<<5);	// SIC = 1
@@ -27,7 +30,7 @@ void i2c_addr(char slave_addr)
 	while(I2C0STAT != 0X18);	
 }
 
-void i2c_addr2(char slave_addr)
+void i2c0_addr2(char slave_addr)
 {
 	I2C0CONSET = (1<<2); // AA =1
 	I2C0CONCLR = (1<<3);	// SIC = 1
@@ -35,7 +38,7 @@ void i2c_addr2(char slave_addr)
 	while(I2C0STAT != 0X40);
 }
 
-void i2c_write(char data)
+void i2c0_write(char data)
 {
 	I2C0CONSET = (1<<2);
 	I2C0CONCLR = (1<<3);	// SIC = 1
@@ -43,14 +46,14 @@ void i2c_write(char data)
 	while(I2C0STAT != 0X28);
 }
 
-char i2c_read()
+char i2c0_read()
 {
 	I2C0CONSET = (1<<2);
 	I2C0CONCLR = (1<<3);	// SIC = 1
 	while(I2C0STAT != 0x50);
 	return I2C0DAT;
 }
-void i2c_location(char loc_addr)
+void i2c0_location(char loc_addr)
 {
 	I2C0CONSET = (1<<2);
 	I2C0CONCLR = (1<<3)|(1<<5);	// SIC = 1 , Clear ST0 ??
@@ -59,69 +62,64 @@ void i2c_location(char loc_addr)
 
 }
 
-void i2c_stop()
-{
-	I2C0CONSET = (1<<2) | (1<<4);	 // AA = 1, STO = 1
-	I2C0CONCLR = (1<<3);			//Clear SIC
 
+
+void i2c0_write_init(char slave_id, char locatn)
+{
+	i2c0_start();
+	i2c0_addr(slave_id & 0XFE);	// Claering 8th bit only, select write operation
+	i2c0_location(locatn);		// Location in EEPROM to use
 }
 
-void i2c_write_init(char slave_id, char locatn)
+void i2c0_dummy_write(char slave_id, char location)
 {
-	i2c_start();
-	i2c_addr(slave_id & 0XFE);	// Claering 8th bit only, select write operation
-	i2c_location(locatn);		// Location in EEPROM to use
-}
+	i2c0_start();
 
-void i2c_dummy_write(char slave_id, char location)
-{
-	i2c_start();
-
-	i2c_addr( slave_id & 0xFE);
+	i2c0_addr( slave_id & 0xFE);
 
 	lcd_str("OK2");
 	delay(500);
 	cmd(0x01);
 
 	
-	i2c_location(location);
-	i2c_stop();
+	i2c0_location(location);
+	i2c0_stop();
 
 } 
 
-void i2c_read_init(char slave_id, char location)
+void i2c0_read_init(char slave_id, char location)
 {
-	i2c_dummy_write(slave_id,location);
+	i2c0_dummy_write(slave_id,location);
 	
-	i2c_start();
-	i2c_addr2(slave_id | 0x01);	
+	i2c0_start();
+	i2c0_addr2(slave_id | 0x01);	
 }
 
 int main()
 {
 	char ch;
 	lcd_init();
-	i2c_init();
-	i2c_write_init(0xA0,0x00);
+	i2c0_init();
+	i2c0_write_init(0xA0,0x00);
 
 	
 	for(ch='A';ch!='D';ch++)
 	{
-		i2c_write(ch);
+		i2c0_write(ch);
 	}
-	i2c_stop();
+	i2c0_stop();
 
 	
 	
 
-	i2c_read_init(0xA0,0x00);
+	i2c0_read_init(0xA0,0x00);
 	
 	
 	
 
 	do
 	{
-		ch = i2c_read();
+		ch = i2c0_read();
 		lcd_char(ch);
 
 	}while(ch!='D');
