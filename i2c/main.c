683e4791f6c89febcdac0bcb787aba5fd5d9e63e
Sarath M <sarathm00@gmail.com>
2015-10-12 10:31:08 +0530

    Not working

--- i2c/main.c
+++ i2c/main.c
@@ -1,22 +1,77 @@
 #include<lpc213x.h>
+#include<stdlib.h>
 #include "lcd.h"
 #include "eeprom.h"
+#include "adc.h"
+#include "uart0_inter.h"
+
+char threshold[5];
+int flag = 0;
+
+void uart(void)__irq // ISR for UART0
+{
+	static int i = 0;
+	char ch;
+	ch = U0RBR; 		// Echo what is recieved
+	
+	uart_tx_char(ch);
+	
+	if(ch=='*')
+	{
+		i=0;
+		flag=0;
+	}
+	else if(ch=='#')
+	{
+		threshold[4]='\0';
+		flag=1;
+	}
+	else
+	{
+		threshold[i++]=ch;		
+	}		
+	
+	VICVectAddr = 0;	// Compulsary in all ISR's
+}
+
 
 int main()
 {
-	char* str;
+	//char* str;
+	int adc_val,thresh_val;
 	lcd_init();
+	adc_init();
+	uart_init();
+	i2c0_init();
+	
+	while(1)
+	{
+	//	if(flag ==1)
+	//	{
+		//	eeprom_write_str(threshold);
+	//		flag=0;
+	//	}
+		
+		adc_val = adc_read();cmd(0x8A);
+		eeprom_read_str();
+		thresh_val = 1234;
+		
+		//cmd(0xc0);
+		//lcd_int(adc_val);
+		//cmd(0xCA);
+		//lcd_int(thresh_val);
 
-   	eeprom_write_str("1234");
-	
-	debug("Data Written!");
-
-	debug("Reading data. .");
-	
-	str = eeprom_read_str();
-	//ch = eeprom_read_char();
-	lcd_str(str);
-	//lcd_char(ch);
-	
-	while(1);
+		if(adc_val == thresh_val)
+		{
+		//	cmd(0x80);
+		//	lcd_str("ALARM!!");
+		}
+		else
+		{
+		//	cmd(0x80);
+		//	lcd_str(". . .");
+		
+		}
+		debug("done1");
+	}			 
 }
