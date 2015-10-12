4b15716b8e535866c3c55de3ac695162798761ca
Sarath M <sarathm00@gmail.com>
2015-10-12 16:06:01 +0530

    Almost working

--- i2c/main.c
+++ i2c/main.c
@@ -20,6 +20,7 @@ void uart(void)__irq // ISR for UART0
 	{
 		i=0;
 		flag=0;
+		threshold[0]='\0';
 	}
 	else if(ch=='#')
 	{
@@ -43,25 +44,22 @@ int main()
 	adc_init();
 	uart_init();
 	i2c_init();
-	
 	while(1)
 	{
-		if(flag ==1)
+		//if(flag ==1)
+		if(flag == 0)
 		{
-			eeprom_write_str(threshold);
+			//eeprom_write_str(threshold);
+			eeprom_write_str("1");
 			flag=0;
 		}
 		
 		adc_val = adc_read();
-		cmd(0x8A);
-		thresh_val = atoi(eeprom_read_str());
-		//thresh_val = 1234;
 		
-		cmd(0xc0);
-		lcd_int(adc_val);
-		cmd(0xCA);
-		lcd_int(thresh_val);
-
+		thresh_val = atoi(eeprom_read_str());
+		
+		//thresh_val = 656;
+		
 		if(adc_val == thresh_val)
 		{
 			cmd(0x80);
@@ -72,6 +70,6 @@ int main()
 			cmd(0x80);
 			lcd_str(". . .");
 		}
-		debug("done1");
+		
 	}			 
 }
