aefc30358d084b1b098001ccabf73595c5adbf8c
Sarath M <sarathm00@gmail.com>
2015-10-13 21:42:07 +0530

    Testing

--- i2c/test2/i2c_test2.c
+++ i2c/test2/i2c_test2.c
@@ -1,78 +1,5 @@
 #include"LPC213x.h"
-void Initialize(void);
 
-/* I 2 C ISR */
-//__irq void I2C _ISR(void);
-void I2C_ISR(void)__irq;	  //Fn Declaration
-
-/* Master Transmitter states */
-void ISR_8(void);
-void ISR_18(void);
-void ISR_28(void);
-/*************************** MAIN ************************/
-
-int main()
-{
-	/* Initialize system */
-	Initialize ();
-	
-	/* Send start bit */
-	I2C0CONSET=0x60; 
-	
-	/* Do forever */
-	while(1);
-//	{
-//		//IOCLR=0x40;
-//		//IOSET=0x40;
-//	}
-}
-
-/*************** System Initialization ***************/
-void Initialize()
-{
-	/* Remap interrupt vectors to SRAM */
-	MEMMAP=0x2;
-	/* Initialize GPIO ports to be used as indicators */
-	//IODIR=0xF0;
-	//IOSET=0xF0; 
-	/* Initialize Pin Connect Block */
-	PINSEL0=0x50;
-	/* Initialize I 2 C */
-	I2C0CONCLR=0x6c; /* clearing all flags */
-	I2C0CONSET=0x40; /* enabling I 2 C */
-	I2C0SCLH=0xC; /* 100 KHz */
-	I2C0SCLL=0xD;
-	/* Initialize VIC for I 2 C use */
-	VICIntSelect=0x0; /* selecting IRQ */
-	VICIntEnable= 0x200;  /* enabling I 2 C */
-	VICVectCntl0= 0x29; /* highest priority and enabled */
-	VICVectAddr0=(unsigned) I2C_ISR;
-
-	/* ISR address written to the respective address register*/
-}
-
-/********************** I 2 C ISR **************************/
-//__irq void I2C_ISR()
-void I2C_ISR(void)__irq
-{
-	int temp=0;
-	temp=I2C0STAT;
-	switch(temp)
-	{
-	case 8:
-	ISR_8();
-	break;
-	case 24:
-	ISR_18();
-	break;
-	case 40:
-	ISR_28();
-	break;
-	default :
-	break;
-	} 
-	VICVectAddr = 0xFF;
-}
 
 /* I 2 C states */
 /* Start condition transmitted */
@@ -115,3 +42,63 @@ void ISR_28()
 	/* Port Indicator */
 	 //IOSET=0x80;
 }
+
+/********************** I 2 C ISR **************************/
+//__irq void I2C_ISR()
+void I2C_ISR(void)__irq
+{
+	int temp=0;
+	temp=I2C0STAT;
+	switch(temp)
+	{
+		case 8:
+			ISR_8();
+			break;
+		
+		case 24:
+			ISR_18();
+			break;
+		
+		case 40:
+			ISR_28();
+			break;
+		
+		default :
+			break;
+	} 
+	
+	//VICVectAddr = 0xFF;  		// Change here
+	VICVectAddr	= 0;
+}
+
+void i2c_init()
+{
+	
+	//MEMMAP = 0x2;				/* Remap interrupt vectors to SRAM */
+	PINSEL0 = 0x50;				/* Initialize Pin Connect Block */
+	I2C0CONCLR = 0XFF; 			/* clearing all flags */
+	//I2C0CONSET = 0x40; 		// Change here 
+	I2C0CONSET = 0X44;		   // ENABLE THE I2C BY SETTING I2EN BIT & ACKNOWEDGE FLAG SET  
+	I2C0SCLH = 0x96; 			/* 100 KHz */
+	I2C0SCLL = 0x96;
+	
+	/* Initialize VIC for I 2 C use */
+	VICIntSelect = 0x0; 				/* selecting IRQ */
+	VICIntEnable = 0x200;  				/* enabling I 2 C */
+	VICVectCntl0 = 0x29; 				/* highest priority and enabled */
+	VICVectAddr0 = (unsigned) I2C_ISR;	/* ISR address written to the respective address register*/
+}
+
+int main()
+{
+	/* Initialize system */
+	i2c_init();
+	
+	/* Send start bit */
+	I2C0CONSET=0x60; 
+	
+	/* Do forever */
+	while(1);
+}
+
+
