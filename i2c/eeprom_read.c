31aa3b8009e4f98e64c6870a6dcc6f181932e9dd
Sarath M <sarathm00@gmail.com>
2015-10-13 21:00:33 +0530

    Working code

---
+++ i2c/eeprom_read.c
@@ -0,0 +1,101 @@
+#include<lpc213x.h>
+#include "lcd.h"
+#include "uart0_inter.h"
+
+unsigned char i,j;
+
+void uart(void)__irq // ISR for UART0
+{
+	uart_tx_char(U0RBR);
+	VICVectAddr = 0;
+}
+void start(void)
+{
+	 I2C0CONSET=0x64;	  //SEND START BIT(STA) & ACKNOWEDGE FLAG SET
+	 I2C0CONCLR=0x08;
+	 while(I2C0STAT!=0X08);
+}
+
+void devadd1(void)
+{
+	I2C0DAT=0XA0;
+	I2C0CONSET=0X04;
+	I2C0CONCLR=0X08;
+	while(I2C0STAT!=0X18);
+}
+
+void devadd2(void)
+{
+	I2C0DAT=0XA1;
+	I2C0CONSET=0X04;
+	I2C0CONCLR=0X08;
+	while(I2C0STAT!=0X40);
+}
+
+void location(int add)
+{
+	I2C0DAT=add;
+	I2C0CONSET=0X04;
+	I2C0CONCLR=0X28;
+	while(I2C0STAT != 0X28);
+}
+
+void stop(void)
+{ 
+	I2C0CONSET=0X14;		  	//SEND STOP BIT(STO) & ACKNOWEDGE FLAG SET
+	I2C0CONCLR=0X48;
+}
+
+char readdata(void)
+{
+	I2C0CONSET=0X04;
+	I2C0CONCLR=0X28;
+	while(I2C0STAT!=0X50);
+	return I2C0DAT; 
+}
+
+void i2c_init()
+{
+	PINSEL0 |= 0X00000050;
+	I2C0CONCLR=0XFF;
+	I2C0CONSET=0X44;		   // ENABLE THE I2C BY SETTING I2EN BIT & ACKNOWEDGE FLAG SET  
+	I2C0SCLL=0X96;
+	I2C0SCLH=0X96;
+	
+}
+
+
+
+void eeprom_read()
+{
+	/***********************WRITING DATA*************************************/
+	start();
+	uart_tx_str("\r\nhere\r\n");
+	devadd1();
+	location(0x00);
+	stop();
+	
+	/******************************************READ********************************************/
+	start();
+	devadd2();
+	for(j=0;j<=5;j++)
+	{
+		uart_tx_char(readdata());
+	}
+
+	stop();
+}
+
+
+int main()
+{
+	i2c_init();
+	uart_init();
+	while(1)
+	{
+		   	uart_tx_str("\r\nReading from eeprom\r\n");
+			eeprom_read();
+			uart_tx_str("\r\nDONE!!\r\n");
+	}
+}
+
