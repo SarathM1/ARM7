82ab2693147f594bd180d5e31c6726653314861f
Sarath M <sarathm00@gmail.com>
2015-10-13 14:57:39 +0530

    Almost working

--- i2c/test3/main3.c
+++ i2c/test3/main3.c
@@ -1,23 +1,156 @@
 #include<lpc213x.h>
-#include<stdlib.h>
 #include "lcd.h"
 #include "uart0_inter.h"
-#include "adc.h"
-#include "i2c.h"
+
+unsigned char i;
 
 void uart(void)__irq // ISR for UART0
 {
-	char ch;
-	ch = U0RBR;
-	uart_tx_char(ch);
+	uart_tx_char(U0RBR);
 	VICVectAddr = 0;
 }
+void start(void)
+{
+	 I2C0CONSET=0x64;	  //SEND START BIT(STA) & ACKNOWEDGE FLAG SET
+	 I2C0CONCLR=0x08;
+	 while(I2C0STAT!=0X08);
+}
+
+void devadd1(void)
+{
+	I2C0DAT=0XA0;
+	I2C0CONSET=0X04;
+	I2C0CONCLR=0X08;
+	while(I2C0STAT!=0X18);
+}
+
+void devadd2(void)
+{
+	I2C0DAT=0XA1;
+	I2C0CONSET=0X04;
+	I2C0CONCLR=0X08;
+	while(I2C0STAT!=0X40);
+}
+
+void location(int add)
+{
+	I2C0DAT=add;
+	I2C0CONSET=0X04;
+	I2C0CONCLR=0X28;
+	while(I2C0STAT != 0X28);
+}
+
+void stop(void)
+{ 
+	I2C0CONSET=0X14;		  	//SEND STOP BIT(STO) & ACKNOWEDGE FLAG SET
+	I2C0CONCLR=0X48;
+}
+
+
+
+char readdata(void)
+{
+	I2C0CONSET=0X04;
+	I2C0CONCLR=0X28;
+	while(I2C0STAT!=0X50);
+	return I2C0DAT; 
+}
+
+void writedata(char ch)
+{
+	I2C0DAT=ch;
+	I2C0CONSET=0X04;
+	I2C0CONCLR=0X08;
+	while(I2C0STAT != 0X28);
+}
+
+void eeprom_write_str(char* str)
+{
+	int k;
+	start();
+	uart_tx_char('?');
+	devadd1();
+	location(0x00);
+	for(k=0;str[k]!='\0';k++)	   //array[i]!='\0';
+	{
+		writedata(str[k]);
+		cmd(0xCA);
+		debug_int(k);
+	}
+	stop();
+	delay(2);		  // delay 2 ms. I2c won't work if removed
+}
+
+void i2c_init()
+{
+	PINSEL0 |= 0X00000050;
+	I2C0CONCLR=0XFF;
+	I2C0CONSET=0X44;		   // ENABLE THE I2C BY SETTING I2EN BIT & ACKNOWEDGE FLAG SET  
+	I2C0SCLL=0X96;
+	I2C0SCLH=0X96;
+	
+}
+
+void eeprom_read_str()
+{
+//	char* str;
+	int j;
+	/***********************WRITING DATA*************************************/
+	start();
+	uart_tx_str("\r\nhere\r\n");
+	devadd1();
+	location(0x00);
+	stop();
+	
+	/******************************************READ********************************************/
+	start();
+	devadd2();
+	for(j=0;j<4;j++)
+	{
+		uart_tx_char(readdata());
+	}
+
+	stop();
+}
+
+
+void eeprom_read()
+{
+	int j;
+	char ch;
+	/***********************WRITING DATA*************************************/
+	start();
+	uart_tx_str("\r\nhere\r\n");
+	devadd1();
+	location(0x00);
+	stop();
+	
+	/******************************************READ********************************************/
+	start();
+	devadd2();
+	for(j=0;j<4;j++)
+	{
+		ch = readdata();
+		uart_tx_char(ch);
+		if(ch == '\0')
+			break;
+	}
+
+	stop();
+}
+
 
 int main()
 {
-	lcd_init();
-	adc_init();
-	uart_init();
 	i2c_init();
-	while(1);
+	uart_init();
+	eeprom_write_str("3489");
+	while(1)
+	{
+		   	uart_tx_str("\r\nReading from eeprom\r\n");
+			eeprom_read();
+			uart_tx_str("\r\nDONE!!\r\n");
+			delay(500);
+	}
 }
+
